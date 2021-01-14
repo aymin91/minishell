57ba87d47dec4d9de28dd3ffbced9c57e6fc734d
gihyun cho <whrlgus33@gmail.com>
2021-01-14 15:54:44 +0900

    parse_backslash_in_dquote 함수 실행 조건 추가

--- src/split_command.c
+++ src/split_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_command.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amin <amin@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: gicho <gicho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 15:09:43 by amin              #+#    #+#             */
-/*   Updated: 2021/01/13 19:39:53 by amin             ###   ########.fr       */
+/*   Updated: 2021/01/14 15:52:01 by gicho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ char		**split_command(char *command, t_list *envs)
 	i = -1;
 	while (token[++i])
 	{
-		if (token[i][0] == '\"' && parse_backslash_in_dquote(&token[i], envs))
-			continue;
-		if ((ft_strlen(token[i]) != 1)
+		if (token[i][0] == '\"' && ft_strchr(token[i], '\\'))
+			parse_backslash_in_dquote(&token[i], envs);
+		else if ((ft_strlen(token[i]) != 1)
 			&& (isin_quote(token[i]) || isin_env(token[i])))
 			token[i] = specify_cmd(token[i], envs);
 	}
